d9186c7f6c1fc68b3a2274a5746165bde997870a
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-15 10:11:17 +0800

    modify

--- pico_w/lesson6_4.py
+++ pico_w/lesson6_4.py
@@ -5,5 +5,6 @@ led25 = Pin("LED",Pin.OUT)
 def second1(t):
     print("過1秒")
     led25.toggle()
-
-tim1 = Timer(period=1000, mode=Timer.PERIODIC, callback=second1)
+    
+tim1 = Timer()
+tim1.init(period=1000, callback=second1)
