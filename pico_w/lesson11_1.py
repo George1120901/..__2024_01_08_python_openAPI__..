7ef5da7f599554f70ac4a48fb0de26f78486078a
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-26 13:37:18 +0800

    modify

--- pico_w/lesson11_1.py
+++ pico_w/lesson11_1.py
@@ -44,7 +44,7 @@ while True:
                 
                 is_press = False
                 
-                url_str = f'https://自已網址/pico_w/{currentTime}?address=chicken_KFC&celsius={temperature}&light={light}'
+                url_str = f'https://openapi-test-6vnu.onrender.com/pico_w/{currentTime}?address=chicken_KFC&celsius={temperature}&light={light}'
                 try:
                     response = urequests.get(url_str)
                     pass
